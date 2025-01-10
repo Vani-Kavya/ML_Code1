36a5d6c01abfd4e8d2feefcbccc31b922957ebb7
Vani-Kavya <jagtap.vanita16@gmail.com>
2025-01-10 19:23:33 +0000

    Laptop Price Prediction Web App

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Laptop Price Prediction Web App")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
